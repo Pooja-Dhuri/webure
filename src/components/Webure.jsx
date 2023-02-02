7e5dad0f77e51dadc4490af6d3b38485b3d7db80
PoojaD12345 <dhuripooja01@gmail.com>
2023-02-02 23:29:03 +0530

    Navbar completed

--- src/components/Webure.jsx
+++ src/components/Webure.jsx
@@ -3,9 +3,63 @@ import React from 'react'
 const Webure = () => {
   return (
     <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        {/* Navbar div */}
+        <div className='flex items-center justify-center space-x-20 mx-auto '>
+            <div>
+            <h3 className='text-red-500 text-2xl italic'>Webure Technologies</h3></div>
+            <div>About Us</div>
+            <div>Services</div>
+            <div>Careers</div>
+            <div>ROI Stories</div>
+            <div>Blog</div>
+            <div>Contact Us</div>
+            <button className='border-2'>Let's talk</button>
+        </div>
+
+        {/* Best business platform div */}
+        <div>
+
+        </div>
+
+        {/* Apps div */}
+        <div>
+
+        </div>
+
+        {/* Services  */}
+        <div>
+
+        </div>
+
+        {/* Why choose us */}
+        <div>
+
+        </div>
+
+        {/* Our solutions */}
+        <div>
+
+        </div>
+
+        {/* percentage  */}
+        <div>
+
+        </div>
+
+        {/* What we do */}
+        <div>
+
+        </div>
+
+        {/* Roles */}
+        <div>
+
+        </div>
+
+        {/* Projects  */}
+        <div>
+
+        </div>
     </div>
   )
 }
