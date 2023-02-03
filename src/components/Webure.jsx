4e4d6a658e212b4c59e4a2f4edc00471bd83421c
PoojaD12345 <dhuripooja01@gmail.com>
2023-02-03 12:19:56 +0530

    get all the content

--- src/components/Webure.jsx
+++ src/components/Webure.jsx
@@ -1,67 +1,177 @@
-import React from 'react'
+import React from "react";
 
 const Webure = () => {
   return (
     <div>
-        {/* Navbar div */}
-        <div className='flex items-center justify-center space-x-20 mx-auto '>
+      {/* Navbar div */}
+      <div className="flex items-center justify-center space-x-20 mx-auto ">
+        <div>
+          <h3 className="text-red-500 text-2xl italic">Webure Technologies</h3>
+        </div>
+        <div>About Us</div>
+        <div>Services</div>
+        <div>Careers</div>
+        <div>ROI Stories</div>
+        <div>Blog</div>
+        <div>Contact Us</div>
+        <button className="border py-3 px-8 rounded-full">Let's talk</button>
+      </div>
+
+      {/* Best business platform div */}
+      <div className="flex justify-space-between items-center">
+        <div className="pl-20 pr-20">
+          <div>
+            <p className="text-orange-500">
+              Best Business Platform - World Record 2021
+            </p>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl py-3 px-8 ">
+              Reach Your Business Goals in Record Time
+            </h1>
+          </div>
+          <div>
+            <h4>
+              Support small business and join the nationwide movement to
+              encourage commercial support for the millions of minority owned
+              businesses helping world economy.
+            </h4>
+          </div>
+          <div className="flex items-center justify-center">
             <div>
-            <h3 className='text-red-500 text-2xl italic'>Webure Technologies</h3></div>
-            <div>About Us</div>
-            <div>Services</div>
-            <div>Careers</div>
-            <div>ROI Stories</div>
-            <div>Blog</div>
-            <div>Contact Us</div>
-            <button className='border-2'>Let's talk</button>
+              <button className="mt-5 rounded-full border border-transparent bg-red-400 py-3 px-8 text-base">
+                Get Started
+              </button>
+            </div>
+            <div>Book a 30 Min Quick Meeting</div>
+          </div>
         </div>
 
-        {/* Best business platform div */}
         <div>
-
+          <img src="1 1.png" alt="business girl" />
         </div>
+      </div>
 
-        {/* Apps div */}
+      {/* Apps div */}
+      <div>
         <div>
-
+          <img src="Group 652.png" alt="" />
         </div>
-
-        {/* Services  */}
         <div>
-
+          <img src="Vector (1).png" alt="" />
         </div>
-
-        {/* Why choose us */}
         <div>
-
+          <img src="Group 282.png" alt="" />
         </div>
-
-        {/* Our solutions */}
         <div>
-
+          <img src="Vector.png" alt="" />
         </div>
+      </div>
 
-        {/* percentage  */}
+      {/* Services  */}
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Why choose us */}
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Our solutions */}
+      <div>
+        <img src="Besnik.png" alt="" />
+      </div>
+
+      {/* percentage  */}
+      <div>
+        <img src="Besnik Marketing Agency Landing Page Countdown.png" alt="" />
+      </div>
+
+      {/* What we do */}
+      <div>
+        <div></div>
         <div>
-
+          <h1>Our Services</h1>
+          <p>
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature
+          </p>
         </div>
+      </div>
 
-        {/* What we do */}
+      {/* Roles */}
+      <div>
         <div>
-
+          <img src="design-thinking (1) 2.png" alt="" />
+          <h1></h1>
         </div>
-
-        {/* Roles */}
         <div>
-
+          <img src="setting (1) 1.png" alt="" />
+          <h1></h1>
         </div>
-
-        {/* Projects  */}
         <div>
-
+          <img src="setting (1) 2.png" alt="" />
+          <h1></h1>
         </div>
+        <div>
+          <img src="setting (1) 3.png" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+          <img src="setting (1) 4.png" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+          <img src="setting (1) 5.png" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+          <img src="setting (1) 6.png" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+          <img src="setting (1) 1.png" alt="" />
+          <h1></h1>
+        </div>
+      </div>
+
+      {/* Projects  */}
+      <div>
+        <div>
+        <img src="" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+        <img src="" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+        <img src="" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+        <img src="" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+        <img src="" alt="" />
+          <h1></h1>
+        </div>
+        <div>
+          <p>Projects</p>
+          <h1>Selected Works from Us</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Webure
+export default Webure;
