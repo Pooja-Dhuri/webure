9ae1b35da5402c23c37b5244591ef55138b1ba7b
PoojaD12345 <dhuripooja01@gmail.com>
2023-02-03 16:19:39 +0530

    completed

--- src/components/Webure.jsx
+++ src/components/Webure.jsx
@@ -4,7 +4,7 @@ const Webure = () => {
   return (
     <div>
       {/* Navbar div */}
-      <div className="flex items-center justify-center space-x-20 mx-auto ">
+      <div className="flex items-center justify-center space-x-20 mx-auto pt-10">
         <div>
           <h3 className="text-red-500 text-2xl italic">Webure Technologies</h3>
         </div>
@@ -18,33 +18,37 @@ const Webure = () => {
       </div>
 
       {/* Best business platform div */}
-      <div className="flex justify-space-between items-center">
-        <div className="pl-20 pr-20">
+      <div className="flex justify-around items-center text-left mb-10 bg-[#FF64890A]">
+        <div>
           <div>
             <p className="text-orange-500">
               Best Business Platform - World Record 2021
             </p>
           </div>
-          <div>
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl py-3 px-8 ">
-              Reach Your Business Goals in Record Time
-            </h1>
+
+          <div className="text-5xl font-bold py-6 ">
+            Reach Your Business<br /> 
+            Goals in Record Time
           </div>
+
           <div>
             <h4>
-              Support small business and join the nationwide movement to
-              encourage commercial support for the millions of minority owned
-              businesses helping world economy.
+              Support small business and join the nationwide movement to<br />
+              encourage commercial support for the millions of minority <br />
+              owned businesses helping world economy.
             </h4>
           </div>
-          <div className="flex items-center justify-center">
+
+          <div className="flex items-center justify-between">
             <div>
-              <button className="mt-5 rounded-full border border-transparent bg-red-400 py-3 px-8 text-base">
+              <button className="mt-5 rounded-full text-white border 
+              border-transparent bg-red-400 py-3 px-8 text-base">
                 Get Started
               </button>
             </div>
             <div>Book a 30 Min Quick Meeting</div>
           </div>
+
         </div>
 
         <div>
@@ -53,33 +57,106 @@ const Webure = () => {
       </div>
 
       {/* Apps div */}
-      <div>
+      <div className="flex justify-center items-center space-x-20 mx-auto ">
         <div>
           <img src="Group 652.png" alt="" />
         </div>
+
         <div>
           <img src="Vector (1).png" alt="" />
         </div>
+
         <div>
           <img src="Group 282.png" alt="" />
         </div>
+
         <div>
           <img src="Vector.png" alt="" />
         </div>
+
       </div>
 
       {/* Services  */}
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="flex justify-center items-center space-x-20 mx-auto">
+
+        <div className="text-left">
+          <p className="text-orange-400">SERVICIES</p>
+          <h1 className="font-bold py-4 text-3xl">Our Capabilities</h1>
+          <p>
+            We will bring the breathe of our<br />experience and industry knowledge 
+            <br /> to help you succeed
+          </p>
+        </div>
+
+        <div>
+          <img src="Group 2060.png" alt="" />
+        </div>
+
+        <div>
+          <img src="Group 2058.png" alt="" />
+        </div>
+
+        <div className="text-left ">
+          <img src="Vector1.png" className="py-3 px-1" alt="" />
+          <h2 className="font-medium py-4 text-2xl">Collaborate</h2>
+          <p className="text-gray-400">
+            Grow the holistic world <br />view of disruptive <br />innovation
+            workspace
+          </p>
+        </div>
+
       </div>
 
       {/* Why choose us */}
-      <div>
-        <div></div>
-        <div></div>
+      <div className="bg-[#282C4B] text-white flex items-center
+       space-x-80 mx-auto pl-20 text-left">
+       
+        <div>
+          <img src="2 1.png" alt="" />
+        </div>
+
+        <div>
+          <div className="text-orange-400">WHY CHOOSE US</div>
+
+          <div className="text-3xl font-bold">
+            Take on any Challenge of <br />
+            the Digital World
+          </div>
+
+          <div>
+            <div className="flex items-center space-x-3 py-7">
+              <img src="Vector select.png" alt="" />
+              <span>Corporate Financial Advisory</span>
+            </div>
+            <div className="flex items-center space-x-3 pb-7">
+              <img src="Vector select.png" alt="" />
+              <span>Development of Financial Models</span>
+            </div>
+            <div className="flex items-center space-x-3 pb-7">
+              <img src="Vector select.png" alt="" />
+              <span>Deal Structuring</span>
+            </div>
+          </div>
+
+          <div className="flex space-x-30 items-center">
+            <div>
+              <input
+                type="text"
+                placeholder="Enter your Email-ID"
+                className="bg-transparent underline "
+              />
+            </div>
+
+            <div>
+              <button className="mb-2 rounded-full border border-transparent
+               bg-red-400 py-2 px-5 mb-10 text-base border-indigo-400 border-solid">
+                Get Started
+              </button>
+            </div>
+
+          </div>
+
+        </div>
       </div>
 
       {/* Our solutions */}
@@ -94,81 +171,114 @@ const Webure = () => {
 
       {/* What we do */}
       <div>
-        <div></div>
-        <div>
-          <h1>Our Services</h1>
-          <p>
+
+        <div className="text-4xl font-bold">What We Do</div>
+
+        <div className="flex items-center justify-around text-left pt-7">
+          <h1 className="text-3xl font-bold">Our Services</h1>
+          <p className="text-2xl text-gray-400">
             It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature
+            making it over 2000 years old.
+            <br /> Richard McClintock, a Latin professor at Hampden-Sydney
+            College in Virginia, looked up one of
+            <br /> the more obscure Latin words, consectetur, from a Lorem Ipsum
+            passage, and going through
+            <br /> the cites of the word in classical literature
           </p>
         </div>
+
       </div>
 
       {/* Roles */}
-      <div>
-        <div>
+      <div className="grid grid-cols-4 gap-4 p-20 place-items-center font-bold">
+
+        <div className="text-center place-content-center text-white 
+        border bg-[#3F0598] rounded-md w-60 h-60 p-10 items-center">
           <img src="design-thinking (1) 2.png" alt="" />
-          <h1></h1>
+          <h1>Design</h1>
         </div>
-        <div>
+
+        <div className=" w-60 h-60 shadow-[0px_0px_18px_rgba(0,0,0,0.12)] 
+        rounded-md p-10">
           <img src="setting (1) 1.png" alt="" />
-          <h1></h1>
+          <h1>Front End Development</h1>
         </div>
-        <div>
+
+        <div className="border w-60 h-60 shadow-[0px_0px_18px_rgba(0,0,0,0.12)]
+         p-10 rounded-md">
           <img src="setting (1) 2.png" alt="" />
-          <h1></h1>
+          <h1>Back End Development</h1>
         </div>
-        <div>
+
+        <div className="border w-60 h-60 p-10 shadow-[0px_0px_18px_rgba(0,0,0,0.12)] 
+         rounded-md">
           <img src="setting (1) 3.png" alt="" />
-          <h1></h1>
+          <h1>Application Development</h1>
         </div>
-        <div>
+
+        <div className="border w-60 h-60 p-10 shadow-[0px_0px_18px_rgba(0,0,0,0.12)] 
+          rounded-md">
           <img src="setting (1) 4.png" alt="" />
-          <h1></h1>
+          <h1>Cloud Services</h1>
         </div>
-        <div>
+
+        <div className="border w-60 h-60 p-10  shadow-[0px_0px_18px_rgba(0,0,0,0.12)] 
+         rounded-md">
           <img src="setting (1) 5.png" alt="" />
-          <h1></h1>
+          <h1>Analytics</h1>
         </div>
-        <div>
+
+        <div className="border w-60 h-60 p-10  shadow-[0px_0px_18px_rgba(0,0,0,0.12)] 
+         rounded-md">
           <img src="setting (1) 6.png" alt="" />
-          <h1></h1>
+          <h1>Q-A And Testing</h1>
         </div>
-        <div>
+
+        <div className="border w-60 h-60 p-10  shadow-[0px_0px_18px_rgba(0,0,0,0.12)] 
+          rounded-md">
           <img src="setting (1) 1.png" alt="" />
-          <h1></h1>
+          <h1>Back Office Support</h1>
         </div>
+
       </div>
 
       {/* Projects  */}
-      <div>
+      <div className="grid grid-cols-3 gap-5 place-items-center font-bold">
+
         <div>
-        <img src="" alt="" />
-          <h1></h1>
+          <img src="Rectangle 1041.png" alt="" />
+          <h1>Cosmetic forcest Marketing</h1>
         </div>
+
         <div>
-        <img src="" alt="" />
-          <h1></h1>
+          <img src="Rectangle 1042.png" alt="" />
+          <h1>Sales Analytic</h1>
         </div>
+
         <div>
-        <img src="" alt="" />
-          <h1></h1>
+          <img src="Rectangle 1043.png" alt="" />
+          <h1>Fashion Store Mobile Apps</h1>
         </div>
+
         <div>
-        <img src="" alt="" />
-          <h1></h1>
+          <img src="Rectangle 1044.png" alt="" />
+          <h1>Laura’s Clore Branding Identity</h1>
         </div>
+
         <div>
-        <img src="" alt="" />
-          <h1></h1>
+          <img src="Rectangle 1045.png" alt="" />
+          <h1>SEO Marketing 4.0</h1>
         </div>
-        <div>
-          <p>Projects</p>
-          <h1>Selected Works from Us</h1>
+
+        <div className="text-left">
+          <p className="text-orange-300">Projects</p>
+          <h1 className="text-3xl">Selected Works<br /> from Us</h1>
+          <button className="mt-5 rounded-full border 
+          border-red-200 py-3 px-8 text-base">
+            see more
+          </button>
         </div>
+        
       </div>
     </div>
   );
